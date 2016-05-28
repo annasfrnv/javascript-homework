6a56739d8051ba1c9c6d5db28e70ed4f81339aca
Anna <anna@safronovs.com>
2016-05-28 23:07:08 +0500

    coded sorting

--- Замыкания, область видимости/script.js
+++ Замыкания, область видимости/script.js
@@ -41,3 +41,49 @@ console.log( buffer() ); // Тест тебя не съест
 buffer.clear();
 
 console.log( buffer() ); // ""
+
+
+
+
+//Сортировка
+var users = [{
+  name: "Вася",
+  surname: 'Иванов',
+  age: 20
+}, {
+  name: "Петя",
+  surname: 'Чапаев',
+  age: 25
+}, {
+  name: "Маша",
+  surname: 'Медведева',
+  age: 18
+}];
+
+
+function byField(field) {
+
+	return function(a, b) {
+      return a[field] > b[field] ? 1 : -1;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
